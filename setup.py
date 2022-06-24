baefea484d52ae19e9ff6206d4267dcd17d72639
Milen Kouylekov <milen.kouylekov@usit.uio.no>
2022-06-24 13:35:39 +0200

    Add the possibility to list only tables with a specific ending.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pysquril',
-    version='0.1.0',
+    version='0.1.1',
     description='Structured query URI language',
     author='Leon du Toit',
     author_email='l.c.d.toit@usit.uio.no',
