fa4b3bb2aafecf9f0334fcea23b3031a75c08927
leondutoit <dutoit.leon@gmail.com>
2022-09-28 09:52:52 +0200

    bump to v0.1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pysquril',
-    version='0.1.2',
+    version='0.1.4',
     description='Structured query URI language',
     author='Leon du Toit',
     author_email='l.c.d.toit@usit.uio.no',
