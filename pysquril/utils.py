46a99c8091e98aa8d912d4364a30e2d025759cb2
leondutoit <dutoit.leon@gmail.com>
2024-05-16 13:34:04 +0200

    use table_audit function in backends

--- pysquril/utils.py
+++ pysquril/utils.py
@@ -8,3 +8,10 @@ def audit_table(table_name: str) -> str:
 
     """
     return table_name + AUDIT_SEPARATOR + AUDIT_SUFFIX
+
+def audit_table_src(audit_table_name: str) -> str:
+    """
+    Construct name of source table for a given audit table.
+
+    """
+    return audit_table_name[:-(len(AUDIT_SEPARATOR + AUDIT_SUFFIX))]
