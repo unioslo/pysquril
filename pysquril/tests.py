325b922565417ba545673f6c484599ac997d0676
leondutoit <dutoit.leon@gmail.com>
2024-05-29 11:25:43 +0200

    use constants to define audit table suffix in tests when excluding tables from listing

--- pysquril/tests.py
+++ pysquril/tests.py
@@ -38,11 +38,12 @@ from pysquril.parser import (
     UriQuery,
 )
 from pysquril.test_data import dataset
-from pysquril.utils import audit_table
+from pysquril.utils import audit_table, AUDIT_SEPARATOR, AUDIT_SUFFIX
 
 
 TEST_REQUESTOR = "p11-treq"
 TEST_REQUESTOR_NAME = "Test Requestor"
+AUDIT_END = f"{AUDIT_SEPARATOR}{AUDIT_SUFFIX}"
 
 class TestParser(object):
 
@@ -225,7 +226,7 @@ class TestBackends(object):
             pass
 
         # test '*' without any tables
-        out = list(db.table_select('*', 'select=count(1)', exclude_endswith = ['_audit', '_metadata']))
+        out = list(db.table_select('*', 'select=count(1)', exclude_endswith = [AUDIT_END, '_metadata']))
         assert list(out) == []
 
         # create tables
@@ -324,24 +325,24 @@ class TestBackends(object):
             print('\n===> BROADCASTING\n')
 
         # broadcasting aggregations
-        out = list(db.table_select('*', 'select=count(1)', exclude_endswith = ['_audit', '_metadata']))
+        out = list(db.table_select('*', 'select=count(1)', exclude_endswith = [AUDIT_END, '_metadata']))
         assert out == [{'another_table': [5]}, {'test_table': [5]}]
 
         # fuzzy matching
-        out = list(db.table_select('another*', 'select=count(1)', exclude_endswith = ['_audit', '_metadata']))
+        out = list(db.table_select('another*', 'select=count(1)', exclude_endswith = [AUDIT_END, '_metadata']))
         assert out == [{'another_table': [5]}]
 
-        out = list(db.table_select('*_table', 'select=count(1)', exclude_endswith = ['_audit', '_metadata']))
+        out = list(db.table_select('*_table', 'select=count(1)', exclude_endswith = [AUDIT_END, '_metadata']))
         assert out == [{'another_table': [5]}, {'test_table': [5]}]
 
         # broadcasting queries without aggregation
-        out = list(db.table_select('*', 'select=x', exclude_endswith = ['_audit', '_metadata']))
+        out = list(db.table_select('*', 'select=x', exclude_endswith = [AUDIT_END, '_metadata']))
         assert out is not None
         assert len(out) == 2
         assert len(out[0].get("another_table")) == 5
         assert len(out[1].get("test_table")) == 5
 
-        out = list(db.table_select('*', 'select=x,y&where=z=not.is.null', exclude_endswith = ['_audit', '_metadata']))
+        out = list(db.table_select('*', 'select=x,y&where=z=not.is.null', exclude_endswith = [AUDIT_END, '_metadata']))
         assert out is not None
         assert len(out) == 2
         assert len(out[0].get("another_table")) == 4
@@ -352,7 +353,7 @@ class TestBackends(object):
             db.table_select(
                 'another_table,test_table',
                 'select=x,y&where=z=not.is.null',
-                exclude_endswith = ['_audit', '_metadata']
+                exclude_endswith = [AUDIT_END, '_metadata']
             )
         )
         assert out is not None
@@ -798,9 +799,9 @@ class TestSqlBackend(unittest.TestCase):
         target = (datetime.datetime.now() - timedelta(days=2)).isoformat()
         if isinstance(self.backend, SqliteBackend):
             new = json.dumps({"timestamp": target})
-            update_query = f"update {audit_table(not_backup_table)} set data = json_patch(data, '{new}')"
+            update_query = f"update {self.backend._fqtn(audit_table(not_backup_table))} set data = json_patch(data, '{new}')"
         elif isinstance(self.backend, PostgresBackend):
-            update_query = f"update {audit_table(not_backup_table)} set data = jsonb_set(data, '{{timestamp}}', '\"{target}\"')"
+            update_query = f"update {self.backend._fqtn(audit_table(not_backup_table))} set data = jsonb_set(data, '{{timestamp}}', '\"{target}\"')"
         with self.session_func(self.engine) as session:
             session.execute(update_query)
 
